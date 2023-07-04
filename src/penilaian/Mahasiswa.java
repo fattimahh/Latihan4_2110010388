e53657912a6b295640f2816b123eb88b7f91354c
fatimahproject <fatimah140903@gmail.com>
2023-07-04 12:28:11 +0700

    Membuat program Latihan 4

--- src/penilaian/Mahasiswa.java
+++ src/penilaian/Mahasiswa.java
@@ -1,5 +1,37 @@
 package penilaian;
 
 public class Mahasiswa {
+    //membuat variabel
+    private double nama, npm;
+
+    @Override
+    public String toString() {
+        return "Mahasiswa{" + "nama=" + nama + ", npm=" + npm + '}';
+    }
+    
+    
+    
+    //constructor
+
+    public Mahasiswa(double nama, double npm) {
+        this.nama = nama;
+        this.npm = npm;
+    }
+
+    public double getNama() {
+        return nama;
+    }
+
+    public double getNpm() {
+        return npm;
+    }
+
+    public void setNama(double nama) {
+        this.nama = nama;
+    }
+
+    public void setNpm(double npm) {
+        this.npm = npm;
+    }
     
 }
--- src/penilaian/NilaiAkhir.java
+++ src/penilaian/NilaiAkhir.java
@@ -1,5 +1,17 @@
 package penilaian;
 
 public class NilaiAkhir {
+    private double uts, uas, tugas;
+
+    public NilaiAkhir(double uts, double uas, double tugas) {
+        this.uts = uts;
+        this.uas = uas;
+        this.tugas = tugas;
+    }
     
+    //membuat method
+    public double setNilaiAkhir() {
+        return 30%*uts + 30%*uas + 40%*tugas;
+    
+}
 }
